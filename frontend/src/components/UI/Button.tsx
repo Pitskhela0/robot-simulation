4e3f7922aee1fa0fcf1348ae45bab1bfa24a42e4
oto812 <abashidzeoto@gmail.com>
2025-06-22 18:52:18 +0400

    created ui components

---
+++ frontend/src/components/UI/Button.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './Button.css';
+
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+};
+
+const Button = ({ children, ...props }: ButtonProps) => {
+  return (
+    <button className="custom-button" {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ frontend/src/components/UI/Input.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import './Input.css';
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+const Input = (props: InputProps) => {
+  return <input className="custom-input" {...props} />;
+};
+
+export default Input;
